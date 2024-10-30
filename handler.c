19cd02b2c6814f90ede8a860810c8d558ff057e8
maboc <maboc@maboc.nl>
2024-10-30 23:33:05 +0100

    eerste stapjes op het handler pad

--- handler.c
+++ handler.c
@@ -2,8 +2,23 @@
 
 void * handler(void * sck){
   int s = *(int *) sck;
-
+  int proceed =0;//0 - proceed 1 - stop proceeding (quit)
+  char * inpbuf;
+  
   output_line(s, "Welcome G10\r\n");
+
+  inpbuf=malloc(1000);
+  while (proceed==0){
+    bzero(inpbuf,1000);
+    read(s, inpbuf,1000);
+
+    if (strncmp(inpbuf, "quit", 1000)==0){
+      proceed=1;
+    } else {
+      write(s, inpbuf,1000);
+    }
+  }
+
   
   return NULL;
 }
