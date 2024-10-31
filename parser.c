c59484458272db38854ad8459596dba8d0017830
maboc <maboc@maboc.nl>
2024-10-31 20:55:01 +0100

    free memory error

--- parser.c
+++ parser.c
@@ -21,15 +21,17 @@ char * commands_get_part(struct dll * command, int gevraagd){
 char * remove_leading_spaces(char * inp){
   char * no_leading_spaces;
   char * tmp;
+  char * sp;
   
   tmp=malloc(strlen(inp)+1);
   bzero(tmp, strlen(inp)+1);
   tmp=strncpy(tmp, inp, strlen(inp));
+  sp=tmp;
   
-  while (strncmp(tmp," ",1)==0) tmp=tmp+1;
+  while (strncmp(sp," ",1)==0) sp=sp+1;
 
-  no_leading_spaces=malloc(strlen(tmp)+1);
-  no_leading_spaces=strncpy(no_leading_spaces, tmp, strlen(tmp)+1);
+  no_leading_spaces=malloc(strlen(sp)+1);
+  no_leading_spaces=strncpy(no_leading_spaces, sp, strlen(sp));
 
   free(tmp);
   
@@ -40,12 +42,15 @@ char * remove_leading_spaces(char * inp){
 struct dll * parse(char * inpbuf){
   char * no_leading_spaces;
   char * no_trailing_spaces;
-  struct dll * parts;
+  struct dll * parts=NULL;
   
   no_leading_spaces=remove_leading_spaces(inpbuf);
-  parts=dll_add(parts, no_leading_spaces);
-  
   free(inpbuf);
+  no_trailing_spaces=remove_trailing_spaces(no_leading_spaces);
+  free(no_leading_spaces);
+  parts=dll_add(parts, no_trailing_spaces);
+  free(no_trailing_spaces);
+
 
 
   return parts;
